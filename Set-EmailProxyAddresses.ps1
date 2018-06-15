4f043b7faae8c0464de96965b723861cabcc38e2
Hector Lopez <hlopez@infoport.es>
2018-06-15 10:11:00 +0200

    Very first version of the script

---
+++ Set-EmailProxyAddresses.ps1
@@ -0,0 +1,8 @@
+﻿# Coded by hlopez
+$ADUsers = Get-ADUser -SearchScope OneLevel -SearchBase "OU=Example,DC=Domain,DC=com" -Filter * -Properties EmailAddress, ProxyAddresses, DistinguishedName
+foreach($User in $ADUsers)
+{
+	$ProxyAddress="SMTP:$($User.EmailAddress)"
+	Set-AdUser -Identity $User.DistinguishedName -Clear ProxyAddresses
+	Set-ADUser -Identity $User.DistinguishedName -Add @{Proxyaddresses=$ProxyAddress}
+}
